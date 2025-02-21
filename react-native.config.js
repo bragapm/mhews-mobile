c08cdf81844f427bd4c1a54111a98f24961730a4
Rama Dhea Yudhistira <90076976+ramdhey@users.noreply.github.com>
2025-02-21 21:00:09 +0700

    IOS Setup

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-    assets: ["./node_modules/react-native-vector-icons/Fonts/"],
+  // assets: ["./node_modules/react-native-vector-icons/Fonts/"],
+  assets: ['./src/assets/fonts/'],
 };
+
+// module.exports = {
+//   dependencies: {
+//     'react-native-vector-icons': {
+//       platforms: {
+//         ios: null,
+//       },
+//     },
+//   },
+//   projects: {
+//     android: {},
+//     ios: {},
+//   },
+//   assets: ['./src/assets/fonts/'],
+// };
